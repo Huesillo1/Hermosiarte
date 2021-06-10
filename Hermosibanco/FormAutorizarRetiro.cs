3f892adc396a9dd54ca92afaa2edc242b214b467
Luis Angel Gonzalez Pelayo <luis.gonzalez7504@alumnos.udg.mx>
2021-06-10 12:33:37 -0500

    Detalles esteticos implementados en form retiros

--- Hermosibanco/FormAutorizarRetiro.cs
+++ Hermosibanco/FormAutorizarRetiro.cs
@@ -275,9 +275,7 @@ namespace Hermosibanco
                 mniAutorizar.Visible = false;
                 mniObservaciones.Visible = false;
                 mnuAutoriza.Visible = false;
-            }
-            
-                
+            }   
         }
     }
 }
