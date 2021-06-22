0352656bc49f938a2cbc904d2e8e69dd4f222d96
Luis Angel Gonzalez Pelayo <luis.gonzalez7504@alumnos.udg.mx>
2021-06-22 11:18:15 -0500

    Agregando parte Miguel

--- Hermosibanco/BasedeDatos.cs
+++ Hermosibanco/BasedeDatos.cs
@@ -25,7 +25,7 @@ namespace Hermosibanco
             string servidor = "127.0.0.1";
             string bd = "hermosiarte";
             string usuario = "root";
-            string password = "user";
+            string password = "";
 
             string cadenaConexion = "Database=" + bd + "; Data Source=" + servidor + "; User Id=" + usuario + "; Password=" + password;
 
---
+++ Hermosibanco/FormBusquedaCompra.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hermosibanco
+{
+    public partial class FormBusquedaCompra : Form
+    {
+        BasedeDatos bd = new BasedeDatos();
+        // USUARIO HARDCODEADO (SE DEBE DE TRAER DESDE EL LOGIN) USAR Settings.Default.usuarioId
+        string usuarioID = "10";
+
+        //PARA ALMACENAR LOS DATOS OBTENIDOS DE LA QUERY
+        string _total;
+        string _fecha_creacion;
+        string _status;
+        string _id;
+
+        public FormBusquedaCompra()
+        {
+            InitializeComponent();
+            string id = IDTextBox.Text.ToString();
+            DataSet ds = new DataSet();
+            //string campos = "id, usuario_id, total, fecha_creacion, status";
+            string tabla = "carrito";
+            string WHERE = "usuario_id = " + usuarioID;
+            try
+            {
+                ds = bd.consult("*", tabla, WHERE, "SI");
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                    {
+                        DatosGridView.Rows.Add();
+                        _total = ds.Tables[0].Rows[i]["total"].ToString();
+                        _fecha_creacion = ds.Tables[0].Rows[i]["fecha_creacion"].ToString();
+                        _status = ds.Tables[0].Rows[i]["status"].ToString();
+                        _id = ds.Tables[0].Rows[i]["id"].ToString();
+
+                        DatosGridView.Rows[i].Cells[0].Value = _id;
+                        //DatosGridView.Rows[i].Cells[1].Value = usuarioID;
+                        DatosGridView.Rows[i].Cells[1].Value = _total;
+                        // SEPARANDO LA FECHA DE LA HORA
+                        string[] sinHora = _fecha_creacion.Split(' ');
+                        // -----------------------------
+                        DatosGridView.Rows[i].Cells[2].Value = sinHora[0];
+                        DatosGridView.Rows[i].Cells[3].Value = _status;
+
+                    }
+
+                }
+                else
+                    MessageBox.Show("No se pudieron recuperar los datos", "Algo salió mal", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString(), "MySQL Exception");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Exception");
+            }
+
+        }
+
+        // BUSQUEDA POR ID
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string _id = IDTextBox.Text.ToString();
+            Form toDetalleCompra = new FormDetalleCompra(_id);
+            toDetalleCompra.Show();
+
+        }
+
+        // BUSQUEDA POR FECHA
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //FECHA
+            string fecha = FiltroFecha.Value.ToString("yyyy-MM-dd");
+
+            string id = IDTextBox.Text.ToString();
+            DataSet ds = new DataSet();
+            string campos = "id, usuario_id, total, fecha_creacion, status";
+            string tabla = "carrito";
+            string WHERE = "fecha_creacion = " + '\u0022' + fecha + '\u0022';
+            Console.WriteLine(WHERE);
+            try
+            {
+                ds = bd.consult("*", tabla, WHERE, "SI");
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    DatosGridView.Rows.Clear();
+                    for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                    {
+                        DatosGridView.Rows.Add();
+                        _total = ds.Tables[0].Rows[i]["total"].ToString();
+                        _fecha_creacion = ds.Tables[0].Rows[i]["fecha_creacion"].ToString();
+
+                        _status = ds.Tables[0].Rows[i]["status"].ToString();
+                        _id = ds.Tables[0].Rows[i]["id"].ToString();
+
+                        DatosGridView.Rows[i].Cells[0].Value = _id;
+                        DatosGridView.Rows[i].Cells[1].Value = _total;
+                        // SEPARANDO LA FECHA DE LA HORA
+                        string[] sinHora = _fecha_creacion.Split(' ');
+                        // -----------------------------
+                        DatosGridView.Rows[i].Cells[2].Value = sinHora[0];
+                        DatosGridView.Rows[i].Cells[3].Value = _status;
+
+                    }
+
+                }
+                else
+                    MessageBox.Show("Datos no encontrados", "Algo salió mal", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString(), "MySQL Exception");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Exception");
+            }
+        }
+
+        // MOSTRAR INFORMACION DE UN CARRITO EN ESPECIFICO
+        private void DatosGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Console.WriteLine("ColumnIndex: " + e.ColumnIndex);
+            Console.WriteLine("RowIndex: " + e.RowIndex);
+
+            // MOSTRAR SIEMPRE EL ID DE LA FILA
+            string idColumna = DatosGridView.Rows[e.RowIndex].Cells[0].Value.ToString();
+            Console.WriteLine(idColumna);
+
+            Form toDetalleCompra = new FormDetalleCompra(idColumna);
+            toDetalleCompra.Show();
+
+        }
+    }
+}
